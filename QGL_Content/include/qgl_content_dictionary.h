993e7f73f8a0a5c6726adf0312d7e98c29a1a015
David Browning <davidbr101@hotmail.com>
2019-05-10 17:34:40 -0700

    Update documentation. Streamlined member data.

--- QGL_Content/include/qgl_content_dictionary.h
+++ QGL_Content/include/qgl_content_dictionary.h
@@ -5,105 +5,159 @@
 
 namespace qgl::content
 {
-   class LIB_EXPORT content_dictionary
-   {
-      public:
-      /*
-       Creates an empty dictionary.
-       */
-      content_dictionary();
+    /*
+     Content files contain a dictionary to lookup data related to the content.
+     A dictionary stores information about data. Each entry contains some
+     metadata about the data, as well as an offset to the data.
+     */
+    class LIB_EXPORT content_dictionary
+    {
+        public:
+        /*
+         Creates an empty dictionary.
+         */
+        content_dictionary();
 
-      /*
-       Creates a dictionary from the header. The dictionary is empty and must be populated
-       using the accesses functions.
-       */
-      content_dictionary(const CONTENT_DICTIONARY_BUFFER& buf);
+        /*
+         Creates a dictionary from the header. The dictionary is empty and must
+         be populated using the accesses functions.
+         */
+        content_dictionary(const CONTENT_DICTIONARY_METADATA_BUFFER& buf);
 
-      content_dictionary(const content_dictionary& r);
+        /*
+         Copy constructor.
+         */
+        content_dictionary(const content_dictionary& r) = default;
 
-      content_dictionary(content_dictionary&& r);
+        /*
+         Move constructor.
+         */
+        content_dictionary(content_dictionary&& r) = default;
 
-      virtual ~content_dictionary();
+        /*
+         Destructor
+         */
+        virtual ~content_dictionary() noexcept = default;
 
-      inline void push_back(const CONTENT_DICTIONARY_ENTRY_BUFFER& entry)
-      {
-         m_entries.push_back(entry);
-      }
+        /*
+         Copies the entry into the dictionary.
+         */
+        void push_back(const CONTENT_DICTIONARY_ENTRY_BUFFER& entry);
 
-      inline void push_back(CONTENT_DICTIONARY_ENTRY_BUFFER&& entry)
-      {
-         m_entries.push_back(entry);
-      }
+        /*
+         Moves the dictionary entry into the dictionary.
+         */
+        void push_back(CONTENT_DICTIONARY_ENTRY_BUFFER&& entry);
+        
+        /*
+         Returns a const reference to the idx'th item in the dictionary.
+         This throws an exception if the index is out of bounds.
+         */
+        inline const CONTENT_DICTIONARY_ENTRY_BUFFER& at(size_t idx) const
+        {
+            return m_entries.at(idx);
+        }
 
-      inline const CONTENT_DICTIONARY_ENTRY_BUFFER& at(size_t idx) const
-      {
-         return m_entries.at(idx);
-      }
+        /*
+         Returns a reference to the idx'th item in the dictionary.
+         This throws an exception if the index is out of bounds.
+         */
+        inline CONTENT_DICTIONARY_ENTRY_BUFFER& at(size_t idx)
+        {
+            return m_entries.at(idx);
+        }
+                
+        /*
+         Returns a const reference to the idx'th item in the dictionary.
+         */
+        inline const CONTENT_DICTIONARY_ENTRY_BUFFER& operator[](
+            size_t idx) const noexcept
+        {
+            return m_entries[idx];
+        }
 
-      inline CONTENT_DICTIONARY_ENTRY_BUFFER& at(size_t idx)
-      {
-         return m_entries.at(idx);
-      }
+        /*
+         Returns a reference to the idx'th item in the dictionary.
+         */
+        inline CONTENT_DICTIONARY_ENTRY_BUFFER& operator[](
+            size_t idx) noexcept
+        {
+            return m_entries[idx];
+        }
 
-      inline const CONTENT_DICTIONARY_ENTRY_BUFFER& operator[](size_t idx) const
-      {
-         return m_entries[idx];
-      }
+        /*
+         Returns a const iterator to the beginning of the dictionary.
+         */
+        inline auto cbegin() const noexcept
+        {
+            return m_entries.cbegin();
+        }
 
-      inline CONTENT_DICTIONARY_ENTRY_BUFFER& operator[](size_t idx)
-      {
-         return m_entries[idx];
-      }
+        /*
+         Returns a const iterator to the end of the dictionary.
+         */
+        inline auto cend() const noexcept
+        {
+            return m_entries.cend();
+        }
 
-      inline auto cbegin() const
-      {
-         return m_entries.cbegin();
-      }
+        /*
+         Returns an iterator to the beginning of the dictionary.
+         */
+        inline auto begin() noexcept
+        {
+            return m_entries.begin();
+        }
 
-      inline auto cend() const
-      {
-         return m_entries.cend();
-      }
+        /*
+         Returns an iterator to the end of the dictionary.
+         */
+        inline auto end() noexcept
+        {
+            return m_entries.end();
+        }
 
-      inline auto begin()
-      {
-         return m_entries.begin();
-      }
+        /*
+         Returns the number of entries in the dictionary.
+         */
+        inline size_t size() const
+        {
+            return m_entries.size();
+        }
 
-      inline auto end()
-      {
-         return m_entries.end();
-      }
+        friend void swap(content_dictionary& first,
+                         content_dictionary& second) noexcept
+        {
+            using std::swap;
+            swap(first.m_entries, second.m_entries);
+        }
 
-      inline const CONTENT_DICTIONARY_BUFFER& buffer() const
-      {
-         return m_buffer;
-      }
+        /*
+         Overwrites this with r and returns a reference to this.
+         */
+        content_dictionary& operator=(content_dictionary r)  noexcept
+        {
+            swap(*this, r);
+            return *this;
+        }
 
-      inline size_t size() const
-      {
-         return m_entries.size();
-      }
+        private:
 
-      friend void swap(content_dictionary& first,
-                       content_dictionary& second) noexcept
-      {
-         using std::swap;
-         swap(first.m_buffer, second.m_buffer);
-         swap(first.m_entries, second.m_entries);
-      }
+        /*
+         Returns a metadata buffer that is used for writing the dictionary
+         to a file.
+         */
+        inline const CONTENT_DICTIONARY_METADATA_BUFFER buffer() const
+        {
+            return CONTENT_DICTIONARY_METADATA_BUFFER(
+                m_entries.size(),
+                sizeof(CONTENT_DICTIONARY_ENTRY_BUFFER),
+                0);
+        }
 
-      content_dictionary& operator=(content_dictionary r)  noexcept
-      {
-         swap(*this, r);
-         return *this;
-      }
-
-      private:
-      CONTENT_DICTIONARY_BUFFER m_buffer;
-      #pragma warning(push)
-      #pragma warning(disable: 4251)
-      std::vector<CONTENT_DICTIONARY_ENTRY_BUFFER> m_entries;
-      #pragma warning(pop)
-   };
+        #pragma warning(push)
+        #pragma warning(disable: 4251)
+        std::vector<CONTENT_DICTIONARY_ENTRY_BUFFER> m_entries;
+        #pragma warning(pop)
+    };
 }
--- QGL_Content/qgl_content_dictionary.cpp
+++ QGL_Content/qgl_content_dictionary.cpp
@@ -2,29 +2,24 @@
 #include "include/qgl_content_dictionary.h"
 
 qgl::content::content_dictionary::content_dictionary() :
-   m_buffer(0, sizeof(CONTENT_DICTIONARY_ENTRY_BUFFER), 0),
-   m_entries(0)
+    m_entries(0)
 {
 }
 
-qgl::content::content_dictionary::content_dictionary(const CONTENT_DICTIONARY_BUFFER& buf) :
-   m_buffer(buf),
-   m_entries(buf.count())
+qgl::content::content_dictionary::content_dictionary(
+    const CONTENT_DICTIONARY_METADATA_BUFFER& buf) :
+    m_entries(buf.count())
 {
 }
 
-qgl::content::content_dictionary::content_dictionary(const content_dictionary& r) :
-   m_buffer(r.m_buffer),
-   m_entries(r.m_entries)
+void qgl::content::content_dictionary::push_back(
+    const CONTENT_DICTIONARY_ENTRY_BUFFER & entry)
 {
+    m_entries.push_back(entry);
 }
 
-qgl::content::content_dictionary::content_dictionary(content_dictionary&& r) :
-   m_buffer(std::move(r.m_buffer)),
-   m_entries(std::move(r.m_entries))
+void qgl::content::content_dictionary::push_back(
+    CONTENT_DICTIONARY_ENTRY_BUFFER && entry)
 {
-}
-
-qgl::content::content_dictionary::~content_dictionary()
-{
+    m_entries.push_back(entry);
 }
