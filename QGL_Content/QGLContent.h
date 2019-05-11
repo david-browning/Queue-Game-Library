42cc38d061906b6125bddde22adedc51b1430637
David Browning <davidbr101@hotmail.com>
2019-05-11 16:45:39 -0700

    Fixed broken include paths.

--- QGL_Content/QGLContent.h
+++ QGL_Content/QGLContent.h
@@ -2,7 +2,7 @@
 #include "include/qgl_content_types.h"
 #include "include/qgl_content_item.h"
 
-#include "include/qgl_content_file.h"
+#include "include/Content-Files/qgl_content_file.h"
 #include "include/qgl_content_dictionary.h"
 #include "include/qgl_file_helpers.h"
 
@@ -11,7 +11,7 @@
 #include "include/Content-Importers/qgl_importer_string.h"
 #include "include/Content-Importers/qgl_importer_wstring.h"
 
-#include "include/Content-Importers/qgl_file_importers.h"
+#include "include/Content-Files/qgl_file_importers.h"
 #include "include/qgl_content_store.h"
 #include "include/qgl_content_project.h"
 #include "include/qgl_content_project_compiler.h"
