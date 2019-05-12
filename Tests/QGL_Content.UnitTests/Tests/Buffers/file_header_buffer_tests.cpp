ace70883f1d03b11093ca714f7f3dc08634c9d4f
David Browning <davidbr101@hotmail.com>
2019-05-12 14:49:30 -0700

    Added test project for QGL_Content

---
+++ Tests/QGL_Content.UnitTests/Tests/Buffers/file_header_buffer_tests.cpp
@@ -0,0 +1,9 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace QGL_Content_UnitTests
+{
+
+}
---
+++ Tests/QGL_Content.UnitTests/pch.h
@@ -0,0 +1,11 @@
+﻿//
+// pch.h
+//
+
+#pragma once
+
+#include <collection.h>
+#include <ppltasks.h>
+#include <QGLContent.h>
+
+#include "UnitTestApp.xaml.h"
