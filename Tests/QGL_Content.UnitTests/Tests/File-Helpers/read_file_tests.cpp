c6db62054dbe16b52722a6be3ca70ef2a619a5d4
David Browning <davidbr101@hotmail.com>
2019-05-12 23:16:26 -0700

    Implemented tests.

--- Tests/QGL_Content.UnitTests/Tests/File-Helpers/read_file_tests.cpp
+++ Tests/QGL_Content.UnitTests/Tests/File-Helpers/read_file_tests.cpp
@@ -2,25 +2,92 @@
 #include "CppUnitTest.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace winrt::Windows::Storage;
+using namespace qgl::content;
 
 namespace QGL_Content_UnitTests
 {
+   /*
+    These tests assume fill_overlapped, fill_security_attributes,
+    fill_createfile_extended_parameters, write_file_sync, open_file_read, 
+    and open_file_write are correct.
+    */
    TEST_CLASS(ReadFileTests)
    {
       public:
+      /*
+       Create a file, write some data to it, close the file, and read it back.
+       */
       TEST_METHOD(ReadEntireFile)
       {
+         auto root = ApplicationData::Current().LocalFolder().Path();
+         winrt::hstring newFilePath(root + L"\\ReadEntireFile.txt");
 
+         auto sa = fill_security_attributes();
+         auto openParams = fill_createfile_extended_parameters(&sa);
+
+         auto handle = open_file_write(newFilePath, openParams);
+         write_file_sync(handle, data_size / 2, 0, data_to_rw);
+         handle.close();
+
+         handle = open_file_read(newFilePath, openParams);
+         auto fileData = file_data(handle);
+
+         Assert::IsTrue(memcmp(fileData.data(), data_to_rw, fileData.size()),
+                        L"The file data is not correct.");
       }
-
+       
+      /*
+       Create a file, write some data to it, close the file, and read the
+       first half back.
+       */
       TEST_METHOD(ReadFileFirstPart)
       {
+         auto root = ApplicationData::Current().LocalFolder().Path();
+         winrt::hstring newFilePath(root + L"\\ReadFileFirstPart.txt");
 
+         auto sa = fill_security_attributes();
+         auto openParams = fill_createfile_extended_parameters(&sa);
+
+         auto handle = open_file_write(newFilePath, openParams);
+         write_file_sync(handle, data_size / 2, 0, data_to_rw);
+         handle.close();
+
+         handle = open_file_read(newFilePath, openParams);
+         char fileData[data_size / 2];
+         read_file_sync(handle, data_size / 2, 0, fileData);
+
+         Assert::IsTrue(memcmp(fileData, data_to_rw, data_size / 2),
+                        L"The file data is not correct.");
       }
 
+      /*
+       Create a file, write some data to it, close the file, and read the
+       second half back.
+       */
       TEST_METHOD(ReadFileSecondPart)
       {
+         auto root = ApplicationData::Current().LocalFolder().Path();
+         winrt::hstring newFilePath(root + L"\\ReadFileSecondPart.txt");
 
+         auto sa = fill_security_attributes();
+         auto openParams = fill_createfile_extended_parameters(&sa);
+
+         auto handle = open_file_write(newFilePath, openParams);
+         write_file_sync(handle, data_size / 2, 0, data_to_rw);
+         handle.close();
+
+         handle = open_file_read(newFilePath, openParams);
+         char fileData[data_size / 2];
+         read_file_sync(handle, data_size / 2, data_size / 2, fileData);
+
+         Assert::IsTrue(memcmp(fileData, data_to_rw + (data_size / 2),
+                               data_size / 2) == 0,
+                        L"The file data is incorrect.");
       }
+
+      private:
+      static const size_t data_size = 10;
+      const char* data_to_rw = "HelloWorld";
    };
 }
--- Tests/QGL_Content.UnitTests/Tests/File-Helpers/write_file_tests.cpp
+++ Tests/QGL_Content.UnitTests/Tests/File-Helpers/write_file_tests.cpp
@@ -2,25 +2,110 @@
 #include "CppUnitTest.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace winrt::Windows::Storage;
+using namespace qgl::content;
 
 namespace QGL_Content_UnitTests
 {
+   /*
+    These tests assume fill_overlapped, fill_security_attributes, 
+    fill_createfile_extended_parameters, open_file_read, and open_file_write
+    are correct.
+    */
    TEST_CLASS(WriteFileTests)
    {
       public:
+      /*
+       Create a file, write some data to it, close the file, re-open it and 
+       read the data.
+       */
       TEST_METHOD(WriteEntireFileAndVerify)
       {
+         auto root = ApplicationData::Current().LocalFolder().Path();
+         winrt::hstring newFilePath(root + L"\\WriteEntireFileAndVerify.txt");
 
+         auto sa = fill_security_attributes();
+         auto openParams = fill_createfile_extended_parameters(&sa);
+
+         auto handle = open_file_write(newFilePath, openParams);
+
+         write_file_sync(handle, data_size, 0, data_to_rw);
+         handle.close();
+
+         handle = open_file_read(newFilePath, openParams);
+
+
+         char* readBuffer[data_size];
+         DWORD bytesRead = 0;
+         auto result = ReadFile(handle.get(), readBuffer, data_size,
+                                &bytesRead, nullptr);
+         Assert::IsTrue(result != 0, L"Could not read the file.");
+
+         Assert::IsTrue(memcmp(readBuffer, data_to_rw, data_size) == 0,
+                        L"The data is not the same.");
       }
 
+      /*
+       Create a file, write some data to it, close the file, re-open it and 
+       read the first half of the data.
+       */
       TEST_METHOD(WriteFirstPartAndVerify)
       {
+         auto root = ApplicationData::Current().LocalFolder().Path();
+         winrt::hstring newFilePath(root + L"\\WriteFirstPartAndVerify.txt");
 
+         auto sa = fill_security_attributes();
+         auto openParams = fill_createfile_extended_parameters(&sa);
+
+         auto handle = open_file_write(newFilePath, openParams);
+         write_file_sync(handle, data_size / 2, 0, data_to_rw);
+         handle.close();
+
+         handle = open_file_read(newFilePath, openParams);
+         char* readBuffer[data_size / 2];
+         DWORD bytesRead = 0;
+         auto result = ReadFile(handle.get(), readBuffer, data_size / 2,
+                                &bytesRead, nullptr);
+
+         Assert::IsTrue(result != 0, L"Could not read the file.");
+
+         Assert::IsTrue(memcmp(readBuffer, data_to_rw, data_size / 2) == 0,
+                        L"The data is not the same.");
       }
 
+      /*
+       Create a file, write some data to it, close the file, re-open it and 
+       read the second half of the data.
+       */
       TEST_METHOD(WriteSecondPartAndVerify)
       {
+         auto root = ApplicationData::Current().LocalFolder().Path();
+         winrt::hstring newFilePath(root + L"\\WriteFirstPartAndVerify.txt");
 
+         auto sa = fill_security_attributes();
+         auto openParams = fill_createfile_extended_parameters(&sa);
+
+         auto handle = open_file_write(newFilePath, openParams);
+         write_file_sync(handle, data_size / 2, data_size / 2, data_to_rw);
+         handle.close();
+
+         handle = open_file_read(newFilePath, openParams);
+         char* readBuffer[data_size / 2];
+         DWORD bytesRead = 0;
+         OVERLAPPED offsetOverlapped;
+         fill_overlapped(data_size / 2, &offsetOverlapped);
+         auto result = ReadFile(handle.get(), readBuffer, data_size/2, 
+                                &bytesRead, &offsetOverlapped);
+
+         Assert::IsTrue(result != 0, L"Could not read the file.");
+
+         Assert::IsTrue(memcmp(readBuffer, data_to_rw + (data_size / 2),
+                               data_size / 2) == 0,
+                        L"The data is not correct.");
       }
+
+      private:
+      static const size_t data_size = 10;
+      const char* data_to_rw = "HelloWorld";
    };
 }
