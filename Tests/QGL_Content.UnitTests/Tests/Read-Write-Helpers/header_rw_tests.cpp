8c8ab9964cc065331a99b44243243db3d6e17436
David Browning <davidbr101@hotmail.com>
2019-05-14 15:59:00 -0700

    Renamed files to match file naming convention.

---
+++ Tests/QGL_Content.UnitTests/Tests/Read-Write-Helpers/header_rw_tests.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace qgl::content;
+
+namespace QGL_Content_UnitTests
+{
+   /*
+    Readings tests involve creating a buffer, writing it using tested APIs and
+    reading using the API to test.
+    Writing tests involve creating a buffer, writing using the API to test, and
+    reading using tested APIs.
+    These tests assume the File-Helper tests.
+    */
+}
