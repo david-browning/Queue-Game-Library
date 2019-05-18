f14402c0aeabfa3748bf50d1d617b047ff1a82e1
David Browning <davidbr101@hotmail.com>
2019-05-18 15:16:41 -0700

    Implement ResourceType projection class.

--- QGL_Projection/include/ResourceType.h
+++ QGL_Projection/include/ResourceType.h
@@ -1,21 +1,36 @@
 #pragma once
 #include "ResourceType.g.h"
+#include <winrt/Windows.Foundation.Collections.h>
+#include <QGLContent.h>
+
+using namespace winrt::Windows::Foundation::Collections;
 
 namespace winrt::QGL_Projection::implementation
 {
-    struct ResourceType : ResourceTypeT<ResourceType>
-    {
-        ResourceType() = default;
+   struct ResourceType : ResourceTypeT<ResourceType>
+   {
+      ResourceType() = delete;
 
-        ResourceType(uint16_t enumValue, hstring const& name);
-        uint16_t Value();
-        Windows::Foundation::Collections::IObservableVector<Windows::Foundation::IInspectable> SupportedLoaders();
-        hstring ToString();
-    };
+      ResourceType(uint16_t enumValue,
+                   hstring const& name,
+                   array_view<uint16_t const> supportedLoaders);
+
+      uint16_t Value();
+
+      com_array<uint16_t> SupportedLoaderIDs();
+
+      hstring ToString();
+
+      private:
+      winrt::hstring m_name;
+      qgl::content::RESOURCE_TYPES m_type;
+      std::vector<uint16_t> m_supportedLoaderIDs;
+   };
 }
 namespace winrt::QGL_Projection::factory_implementation
 {
-    struct ResourceType : ResourceTypeT<ResourceType, implementation::ResourceType>
-    {
-    };
+   struct ResourceType : ResourceTypeT<ResourceType,
+      implementation::ResourceType>
+   {
+   };
 }
--- QGL_Projection/src/Projections/Content/Content-Metadata/ResourceType.cpp
+++ QGL_Projection/src/Projections/Content/Content-Metadata/ResourceType.cpp
@@ -3,20 +3,27 @@
 
 namespace winrt::QGL_Projection::implementation
 {
-    ResourceType::ResourceType(uint16_t enumValue, hstring const& name)
-    {
-        throw hresult_not_implemented();
-    }
-    uint16_t ResourceType::Value()
-    {
-        throw hresult_not_implemented();
-    }
-    Windows::Foundation::Collections::IObservableVector<Windows::Foundation::IInspectable> ResourceType::SupportedLoaders()
-    {
-        throw hresult_not_implemented();
-    }
-    hstring ResourceType::ToString()
-    {
-        throw hresult_not_implemented();
-    }
+   ResourceType::ResourceType(uint16_t enumValue,
+                              hstring const& name,
+                              array_view<uint16_t const> supportedLoaders) :
+      m_type(static_cast<qgl::content::RESOURCE_TYPES>(enumValue)),
+      m_name(name),
+      m_supportedLoaderIDs(supportedLoaders.begin(), supportedLoaders.end())
+   {
+   }
+
+   uint16_t ResourceType::Value()
+   {
+      return m_type;
+   }
+
+   com_array<uint16_t> ResourceType::SupportedLoaderIDs()
+   {
+      return com_array<uint16_t>(m_supportedLoaderIDs);
+   }
+
+   hstring ResourceType::ToString()
+   {
+      return m_name;
+   }
 }
