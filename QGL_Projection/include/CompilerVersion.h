e051b9edd9a7696c0c27abb118a58b4d29a58afd
David Browning <davidbr101@hotmail.com>
2019-05-18 14:19:51 -0700

    Implement CompilerVersion runtime class.

--- QGL_Projection/include/CompilerVersion.h
+++ QGL_Projection/include/CompilerVersion.h
@@ -1,20 +1,36 @@
 #pragma once
 #include "CompilerVersion.g.h"
+#include <QGLModel.h>
+
+using namespace winrt::Windows::Foundation::Collections;
 
 namespace winrt::QGL_Projection::implementation
 {
-    struct CompilerVersion : CompilerVersionT<CompilerVersion>
-    {
-        CompilerVersion() = default;
+   struct CompilerVersion : CompilerVersionT<CompilerVersion>
+   {
+      CompilerVersion() = default;
 
-        CompilerVersion(uint32_t val, hstring const& str);
-        uint32_t Value();
-        hstring ToString();
-    };
+      CompilerVersion(uint32_t val, hstring const& str);
+
+      uint32_t Value();
+
+      hstring ToString();
+
+      static QGL_Projection::CompilerVersion Version_0_1();
+
+      static QGL_Projection::CompilerVersion Version_0_2();
+      
+      static IVector<QGL_Projection::CompilerVersion> CompilerVersions();
+
+      private:
+      qgl::qgl_version_t m_version;
+      winrt::hstring m_name;
+   };
 }
 namespace winrt::QGL_Projection::factory_implementation
 {
-    struct CompilerVersion : CompilerVersionT<CompilerVersion, implementation::CompilerVersion>
-    {
-    };
+   struct CompilerVersion : CompilerVersionT<CompilerVersion,
+      implementation::CompilerVersion>
+   {
+   };
 }
--- QGL_Projection/src/Projections/Model/Versioning/CompilerVersion.cpp
+++ QGL_Projection/src/Projections/Model/Versioning/CompilerVersion.cpp
@@ -3,16 +3,41 @@
 
 namespace winrt::QGL_Projection::implementation
 {
-    CompilerVersion::CompilerVersion(uint32_t val, hstring const& str)
-    {
-        throw hresult_not_implemented();
-    }
-    uint32_t CompilerVersion::Value()
-    {
-        throw hresult_not_implemented();
-    }
-    hstring CompilerVersion::ToString()
-    {
-        throw hresult_not_implemented();
-    }
+   CompilerVersion::CompilerVersion(uint32_t val, hstring const& str) :
+      m_version(val),
+      m_name(str)
+   {
+
+   }
+
+   uint32_t CompilerVersion::Value()
+   {
+      return m_version;
+   }
+
+   hstring CompilerVersion::ToString()
+   {
+      return m_name;
+   }
+
+   QGL_Projection::CompilerVersion CompilerVersion::Version_0_1()
+   {
+      return QGL_Projection::CompilerVersion(qgl::QGL_VERSION_0_1_WIN,
+                                             L"0.1");
+   }
+
+   QGL_Projection::CompilerVersion CompilerVersion::Version_0_2()
+   {
+      return QGL_Projection::CompilerVersion(qgl::QGL_VERSION_0_2_WIN,
+                                             L"0.2");
+   }
+
+   IVector<QGL_Projection::CompilerVersion> CompilerVersion::CompilerVersions()
+   {
+      return winrt::single_threaded_vector<QGL_Projection::CompilerVersion>(
+         {
+            CompilerVersion::Version_0_1(),
+            CompilerVersion::Version_0_2()
+         });
+   }
 }
