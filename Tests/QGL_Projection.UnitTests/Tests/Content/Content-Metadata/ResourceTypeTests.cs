587d320ea201e782b3775044bbcb10b2ab3dddb1
David Browning <davidbr101@hotmail.com>
2019-05-18 17:03:06 -0700

    Added test projection for QGL Projection.

---
+++ Tests/QGL_Projection.UnitTests/Tests/Content/Content-Metadata/ResourceTypeTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace QGL_Projection.UnitTests.Tests.Content.Content_Metadata
+{
+    [TestClass]
+    public class ResourceTypeTests
+    {
+    }
+}
---
+++ Tests/QGL_Projection.UnitTests/Tests/Model/Versioning/CompilerVersionTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace QGL_Projection.UnitTests.Tests.Model.Versioning
+{
+    [TestClass]
+    public class CompilerVersionTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
