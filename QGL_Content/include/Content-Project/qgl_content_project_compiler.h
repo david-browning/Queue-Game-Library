7b842996df4c92739c8419e82743102ac953e79d
David Browning <davidbr101@hotmail.com>
2019-05-19 17:54:37 -0700

    Update documentation.

--- QGL_Content/include/Content-Project/qgl_content_project_compiler.h
+++ QGL_Content/include/Content-Project/qgl_content_project_compiler.h
@@ -14,6 +14,7 @@ namespace qgl::content
 
    /*
     Compiles a content project into a content file and saves it.
+    Storage file must have write permissions.
     Storage file permissions should be brokered by the storage file provider.
     */
    extern LIB_EXPORT void compile_project(
