7422db7c3cb2ce667703bd1233a2b0d14084a6d6
David Browning <davidbr101@hotmail.com>
2019-05-19 17:11:46 -0700

    Declare code to convert content projects into content files.

--- QGL_Content/include/Content-Project/qgl_content_project_compiler.h
+++ QGL_Content/include/Content-Project/qgl_content_project_compiler.h
@@ -1,11 +1,35 @@
 #pragma once
 #include "include/qgl_content_include.h"
+#include "include/Content-Project/qgl_content_project.h"
+#include "include/qgl_file_helpers.h"
 
 namespace qgl::content
 {
    /*
+    Compiles a content project into a content file and saves it.
+    hndl: Handle to a file where the content project will be compiled.
+    The handle must have write permissions. This does not close the handle. 
+    */
+   void LIB_EXPORT compile_project(const content_project& proj,
+                                   const winrt::file_handle& hndl);
+
+   /*
     absPath: Path where to save the compiled project.
     */
-   //void LIB_EXPORT compile_project(content_project& proj,
-   //                                const winrt::hstring& absPath);
+   void compile_project(const content_project& proj,
+                        const winrt::hstring& absPath)
+   {
+      auto handle = open_file_write(absPath);
+      compile_project(proj, handle);
+   }
+
+   /*
+    Storage file permissions should be brokered by the storage file provider.
+    */
+   void compile_project(const content_project& proj,
+                        const winrt::Windows::Storage::StorageFile& f)
+   {
+      auto handle = open_file_write(f);
+      compile_project(proj, handle);
+   }
 }
