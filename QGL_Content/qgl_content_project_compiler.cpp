fc519a35c9b98653f13a0c5e7a4fca7e60b75d7c
David Browning <davidbr101@hotmail.com>
2019-05-19 17:40:20 -0700

    Declared new signatures to compile a content file from a content project.

--- QGL_Content/qgl_content_project_compiler.cpp
+++ QGL_Content/qgl_content_project_compiler.cpp
@@ -1,2 +1,16 @@
 #include "pch.h"
 #include "include\Content-Project\qgl_content_project_compiler.h"
+
+void qgl::content::compile_project(const content_project& proj,
+                                   const winrt::hstring& absPath)
+{
+   throw winrt::hresult_not_implemented();
+}
+
+
+void qgl::content::compile_project(
+   const content_project& proj,
+   const winrt::Windows::Storage::StorageFile& f)
+{
+   throw winrt::hresult_not_implemented();
+}
