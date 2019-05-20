4bc869f3d021073980421b2b5702b4bb3ff08896
David Browning <davidbr101@hotmail.com>
2019-05-19 21:04:11 -0700

    Implement project compiler code.

--- QGL_Content/qgl_content_project_compiler.cpp
+++ QGL_Content/qgl_content_project_compiler.cpp
@@ -1,16 +1,41 @@
 #include "pch.h"
 #include "include\Content-Project\qgl_content_project_compiler.h"
+#include "include/Content-Files/qgl_content_file.h"
 
-void qgl::content::compile_project(const content_project& proj,
-                                   const winrt::hstring& absPath)
+namespace qgl::content
 {
-   throw winrt::hresult_not_implemented();
+   void compile_content_project(const content_project& proj,
+                                content_file<true>& cf)
+   {
+      CONTENT_FILE_HEADER_BUFFER hdr(proj.metadata());
+      cf.header() = hdr;
+
+      //Fill the dictionary and 
+      for (const auto& entry : proj)
+      {
+         auto bufferHandle = open_file_read(entry.second);
+         auto buffer = file_data(bufferHandle);
+         cf.emplace_back(entry.first, buffer);
+      }
+
+      cf.flush();
+   }
+}
+
+void qgl::content::compile_content_project(const content_project& proj,
+                                           const winrt::hstring& absPath)
+{
+   //Create a content file in write mode.
+   content_file<true> cf(absPath);
+   compile_content_project(proj, cf);
 }
 
 
-void qgl::content::compile_project(
+void qgl::content::compile_content_project(
    const content_project& proj,
    const winrt::Windows::Storage::StorageFile& f)
 {
-   throw winrt::hresult_not_implemented();
+   //Create a content file in write mode.
+   content_file<true> cf(f);
+   compile_content_project(proj, cf);
 }
