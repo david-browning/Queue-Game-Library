2ec6f3e7af2c1ddd3953bd32e6baa17c305d9adc
David Browning <davidbr101@hotmail.com>
2019-05-21 14:15:48 -0700

    Test Refactoring

---
+++ Tests/QGL_Content.UnitTests/Tests/content_file_tests.cpp
@@ -0,0 +1,10 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace winrt::Windows::Storage;
+using namespace qgl::content;
+
+namespace QGL_Content_UnitTests
+{
+}
