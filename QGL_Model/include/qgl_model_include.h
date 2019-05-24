d1a9228a810207b6aea53619428a31b8ec9a6598
David Browning <davidbr101@hotmail.com>
2019-05-24 13:13:09 -0700

    DLL exports will no longer use LIB_EXPORT. Instead, use QGL_MODE_API.

--- QGL_Model/include/qgl_model_include.h
+++ QGL_Model/include/qgl_model_include.h
@@ -8,3 +8,9 @@
 #include <Windows.h>
 #include <winrt/base.h>
 #include <winrt/Windows.Foundation.h>
+
+#ifdef QGL_MODEL_EXPORTS
+#define QGL_MODEL_API __declspec(dllexport)
+#else
+#define QGL_MODEL_API __declspec(dllimport)
+#endif
