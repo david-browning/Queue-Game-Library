0e00bf1e015e35ad67ee4fe1e8056ac78e17fabe
David Browning <davidbr101@hotmail.com>
2019-05-26 15:18:39 -0700

    Added test.

--- Tests/QGL_Projection.UnitTests/Tests/Content/Content-Project/ContentProjectTests.cs
+++ Tests/QGL_Projection.UnitTests/Tests/Content/Content-Project/ContentProjectTests.cs
@@ -13,6 +13,9 @@ namespace QGL_Projection.UnitTests.Tests.Content.Content_Project
     [TestClass]
     public class ContentProjectTests
     {
+        /*
+         Test to make sure writing a project does not throw an exception.
+         */
         [TestMethod]
         public async Task SaveProject()
         {
@@ -32,7 +35,62 @@ namespace QGL_Projection.UnitTests.Tests.Content.Content_Project
             });
         }
 
+        [TestMethod]
+        public async Task SaveReadProject()
+        {
+            var storageFolder = ApplicationData.Current.LocalFolder;
+
+            var storageFile = await storageFolder.CreateFileAsync(
+                "SaveReadProjectedContentProject.txt",
+                CreationCollisionOption.ReplaceExisting);
+
+            await dispatcher.RunAsync(CoreDispatcherPriority.Normal,
+            () =>
+            {
+                //Create a new project.
+                QGL_Projection.ContentProject contentProject =
+                    new ContentProject();
+
+                ContentMetadata newProjectMeta = new ContentMetadata();
+                newProjectMeta.ContentName = "New Project";
+                newProjectMeta.CompilerVersion = 
+                    CompilerVersion.VersionLatest.Value;
+                newProjectMeta.ContentLoader = 1;
+                newProjectMeta.ResourceType = 2;
+                contentProject.Metadata = newProjectMeta;
+
+                ContentMetadata blueMetadata = new ContentMetadata();
+                blueMetadata.ContentName = "Blue Float";
+                ContentProjectEntry blueEntry = new ContentProjectEntry(
+                    storageFolder.Path + "\\projectedBlue.txt",
+                    blueMetadata);
+
+                ContentMetadata redMetadata = new ContentMetadata();
+                redMetadata.ContentName = "Red Float";
+                ContentProjectEntry redEntry = new ContentProjectEntry(
+                    storageFolder.Path + "\\projectedRed.txt",
+                    redMetadata);
+
+                ContentMetadata greenMetadata = new ContentMetadata();
+                greenMetadata.ContentName = "Green Float";
+                ContentProjectEntry greenEntry = new ContentProjectEntry(
+                    storageFolder.Path + "\\projectedGreen.txt",
+                    greenMetadata);
+
+                contentProject.Entries.Add(redEntry);
+                contentProject.Entries.Add(greenEntry);
+                contentProject.Entries.Add(blueEntry);
+
+                contentProject.SaveProjectFile(storageFile);
+
+                ContentProject readProject = new ContentProject();
+                readProject.LoadFromFileAsync(storageFile);
+            });
+        }
+
         private Windows.UI.Core.CoreDispatcher dispatcher =
             Windows.ApplicationModel.Core.CoreApplication.MainView.CoreWindow.Dispatcher;
     }
+
+   
 }
