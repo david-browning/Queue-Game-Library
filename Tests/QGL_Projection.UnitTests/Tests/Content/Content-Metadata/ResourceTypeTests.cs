6313e6d04756f08f25536973bbe13077c6a7a57d
David Browning <davidbr101@hotmail.com>
2019-05-25 17:23:40 -0700

    Test ResourceType.

--- Tests/QGL_Projection.UnitTests/Tests/Content/Content-Metadata/ResourceTypeTests.cs
+++ Tests/QGL_Projection.UnitTests/Tests/Content/Content-Metadata/ResourceTypeTests.cs
@@ -11,5 +11,28 @@ namespace QGL_Projection.UnitTests.Tests.Content.Content_Metadata
     [TestClass]
     public class ResourceTypeTests
     {
+        [TestMethod]
+        public void Construct()
+        {
+            var loaders = new ushort[]
+            {
+                0, 1, 2
+            };
+
+            ResourceType r = new ResourceType(1, "Some Name", loaders);
+            Assert.AreEqual("Some Name", r.ToString(),
+                "The name is not correct.");
+
+            Assert.AreEqual(1, r.Value,
+                "The resource type value is not correct.");
+
+            var q = from a1 in loaders
+                    join a2 in r.SupportedLoaderIDs on a1 equals a2
+                    select a2;
+
+            Assert.IsTrue(r.SupportedLoaderIDs.Length == loaders.Length &&
+                q.Count() == loaders.Length,
+                "The list is not correct.");
+        }
     }
 }
