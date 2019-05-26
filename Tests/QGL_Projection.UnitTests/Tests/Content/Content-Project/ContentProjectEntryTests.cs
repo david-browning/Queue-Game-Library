f7b28e7faa511f5a73685cd9b9e3e825f5428f15
David Browning <davidbr101@hotmail.com>
2019-05-25 22:52:29 -0700

    Tests for a content project entry projection.

--- Tests/QGL_Projection.UnitTests/Tests/Content/Content-Project/ContentProjectEntryTests.cs
+++ Tests/QGL_Projection.UnitTests/Tests/Content/Content-Project/ContentProjectEntryTests.cs
@@ -5,12 +5,85 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using QGL_Projection;
+using Windows.UI.Core;
 
 namespace QGL_Projection.UnitTests.Tests.Content.Content_Project
 {
     [TestClass]
     public class ContentProjectEntryTests
     {
+        [TestMethod]
+        public void Construct()
+        {
+            var metadata = new ContentMetadata();
+            ContentProjectEntry e1 = new ContentProjectEntry("C:\\Test.txt",
+                metadata);
 
+            Assert.AreEqual(metadata, e1.Metadata,
+                "The metadata is not correct.");
+
+            Assert.AreEqual("C:\\Test.txt", e1.FilePath,
+                "The file path is not correct.");
+        }
+
+        [TestMethod]
+        public async Task GetSetFilePath()
+        {
+            await dispatcher.RunAsync(CoreDispatcherPriority.Normal,
+            () =>
+            {
+                ContentProjectEntry e1 = new ContentProjectEntry("C:\\Test.txt",
+                        new ContentMetadata());
+
+                Assert.AreEqual("C:\\Test.txt", e1.FilePath,
+                    "The file path is not correct.");
+
+                var newPath = "D:\\TestFile.txt";
+                e1.FilePath = newPath;
+
+                Assert.AreEqual(newPath, e1.FilePath);
+            });
+        }
+
+        [TestMethod]
+        public async Task GetSetMetadata()
+        {
+            await dispatcher.RunAsync(CoreDispatcherPriority.Normal,
+            () =>
+            {
+                ContentProjectEntry e1 = new ContentProjectEntry("C:\\Test.txt",
+                        new ContentMetadata());
+
+                e1.Metadata.ResourceType = 1;
+                Assert.AreEqual(1, e1.Metadata.ResourceType,
+                    "The resource type is not correct.");
+
+                var newMetadata = new ContentMetadata();
+                newMetadata.ContentName = "New Content Name";
+
+                e1.Metadata = newMetadata;
+                Assert.AreEqual(newMetadata, e1.Metadata,
+                    "The metadata is not correct.");
+            });
+        }
+
+        [TestMethod]
+        public async Task ToStringTest()
+        {
+            await dispatcher.RunAsync(CoreDispatcherPriority.Normal,
+            () =>
+            {
+                var metadata = new ContentMetadata();
+                metadata.ContentName = "Content Name";
+                ContentProjectEntry e1 = new ContentProjectEntry("C:\\Test.txt",
+                    metadata);
+
+                Assert.AreEqual("Content Name", e1.ToString(),
+                    "The ToString() is not correct.");
+            });
+        }
+
+        private Windows.UI.Core.CoreDispatcher dispatcher =
+          Windows.ApplicationModel.Core.CoreApplication.MainView.CoreWindow.Dispatcher;
     }
 }
