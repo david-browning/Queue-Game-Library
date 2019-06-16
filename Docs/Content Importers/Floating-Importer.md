591b567f619f54efd4cc7c9ef0fde9356cb52da0
David Browning <davidbr101@hotmail.com>
2019-06-15 19:25:32 -0700

    Update rational. Added rational_importer.

---
+++ Docs/Content Importers/Floating-Importer.md
@@ -0,0 +1,12 @@
+Floating Point Importers
+========================
+
+## Description:
+Floats and doubles may be interpreted on different platforms. To keep floating
+point numbers consistent across content files, they are encoded as a rational
+number.
+
+## Binary Layout:
+Byte 0: Number of bytes large the numerator is.
+Bytes 1 through N: Numerator encoded as a signed integer.
+Bytes N + 1 though 2N: Numerator encoded as a signed integer.
--- QGL_Model/include/qgl_rational.h
+++ QGL_Model/include/qgl_rational.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "include/qgl_model_include.h"
+#include <type_traits>
 
 namespace qgl::math
 {
@@ -10,54 +11,166 @@ namespace qgl::math
    }
 
    #pragma pack(push, 1)
-   struct QGL_MODEL_API rational
+   template<typename NumberT>
+   struct rational
    {
-      public:
-      using number_t = int32_t;
+      static_assert(std::is_integral<NumberT>::value, 
+                    "NumberT must be integral.");
 
+      static_assert(std::is_signed<NumberT>::value,
+                    "NumberT must be signed.");
+
+      public:
       /*
        Rational number evaluates to zero.
        */
-      constexpr rational();
+      constexpr rational() :
+         m_den(1),
+         m_num(0)
+      {
+
+      }
 
       /*
        Throws std::invalid_argument if denominator is 0.
        */
-      constexpr rational(number_t num, number_t den);
+      constexpr rational(NumberT num, NumberT den) :
+         m_den(den),
+         m_num(num)
+      {
+         if (den == 0)
+         {
+            throw std::invalid_argument("The denominator cannot be zero.");
+         }
 
+         simplify();
+      }
+
+      /*
+       Copy constructor
+       */
       rational(const rational&) = default;
 
+      /*
+       Move constructor.
+       */
       rational(rational&&) = default;
 
+      /*
+       Destructor
+       */
       ~rational() noexcept = default;
 
-      friend void swap(rational& l, rational& r) noexcept;
-      rational& operator=(rational r) noexcept;
-      rational& operator=(rational&& r);
+      /*
+       Swaps the content of l and r.
+       */
+      friend void swap(rational& l, rational& r) noexcept
+      {
+         using std::swap;
+         swap(l.m_num, r.m_num);
+         swap(l.m_den, r.m_den);
+      }
 
-      friend bool operator==(const rational& l, const rational& r) noexcept;
-      friend bool operator!=(const rational& l, const rational& r) noexcept;
+      /*
+       Assignment operator.
+       */
+      rational& operator=(rational r) noexcept
+      {
+         swap(*this, r);
+         return *this;
+      }
 
-      float operator()() const noexcept;
+      /*
+       Move assign operator.
+       */
+      rational& operator=(rational&& r) = default;  
+      
+      /*
+       Returns a floating point representation of the rational number.
+       */
+      operator float() const noexcept
+      {
+         return static_cast<float>(m_num) / static_cast<float>(m_den);
+      }
 
+      operator double() const noexcept
+      {
+         return static_cast<double>(m_num) / static_cast<double>(m_den);
+      }
+
+      /*
+       Adds two rational numbers.
+       */
       //friend rational operator+(const rational& l, const rational& r);
+
+      /*
+       Subtracts two rational numbers.
+       */
       //friend rational operator-(const rational& l, const rational& r);
+
+      /*
+       Multiplies two rational numbers.
+       */
       //friend rational operator*(const rational& l, const rational& r);
+
+      /*
+       Returns l / r.
+       */
       //friend rational operator/(const rational& l, const rational& r);
 
+      /*
+       Returns the rational number * -1.
+       */
       //rational& operator-() noexcept;
 
+      /*
+       Returns true l and r evaluate to the same value.
+       */
+      //friend bool operator==(const rational& l, const rational& r) noexcept;
+
+      /*
+       Returns false if l and r evaluate to the same value.
+       */
+      friend bool operator!=(const rational& l, const rational& r) noexcept
+      {
+         return !(l == r);
+      }
+
+      /*
+       Returns true if the value of l is less than the value of r.
+       */
       //friend bool operator<(const rational& l, rational& r) noexcept;
+      
+      /*
+       Returns true if the value of l is greater than the value of r.
+       */
       //friend bool operator>(const rational& l, rational& r) noexcept;
+      
+      /*
+       Returns true if the value of l is less than or equal to the value of r.
+       */
       //friend bool operator<=(const rational& l, rational& r) noexcept;
-      //friend bool operator>=(const rational& l, rational& r) noexcept;
+      
+      /*
+       Returns true if the value of l is greater than or equal to the value 
+       of r.
+       */
+       //friend bool operator>=(const rational& l, rational& r) noexcept;
 
       private:
 
-      constexpr void simplify();
+      /*
+       Simplifies the rational number.
+       */
+      constexpr void simplify()
+      {
+         auto g = gcd(m_num, m_den);
+         m_num = m_num / g;
+         m_den = m_den / g;
+      }
       
-      number_t m_num;
-      number_t m_den;
+      NumberT m_num;
+      NumberT m_den;
    };
    #pragma pack(pop)
 }
