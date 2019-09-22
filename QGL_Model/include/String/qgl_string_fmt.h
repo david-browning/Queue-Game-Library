6f36a581981b891ff071c389d2b846e485359bae
David Browning <davidbr101@hotmail.com>
2019-09-22 14:47:12 -0700

    New string helpers. Memory helper implementations use new string code.

--- QGL_Model/include/String/qgl_string_fmt.h
+++ QGL_Model/include/String/qgl_string_fmt.h
@@ -1,7 +1,56 @@
 #pragma once
 #include "include/qgl_model_include.h"
 #include "include/String/qgl_string.h"
+#include <sstream>
 
 namespace qgl
 {
+   /*
+    Writes the null-terminated string to the out stream.
+    Returns the stream.
+    */
+   template<class Stream>
+   static Stream& toprintf(Stream& ostrm, const sys_char* fmt)
+   {
+      ostrm << fmt;
+      return ostrm;
+   }
+
+   /*
+    
+    */
+   template<class Stream, typename Param, typename... Params>
+   static Stream& toprintf(Stream& ostrm,
+                    const sys_char* fmt,
+                    const Param& arg,
+                    const Params&... args)
+   {
+      while (*fmt != TEXT('\0'))
+      {
+         const auto c = *fmt;
+         if (c == TEXT('%'))
+         {
+            ostrm << arg;
+            return toprintf<Stream>(ostrm, fmt + 1, args...);
+         }
+
+         ostrm << c;
+         fmt++;
+      }
+
+      return ostrm;
+   }
+
+   static sys_str tsprintf(const sys_char* fmt)
+   {
+      return sys_str(fmt);
+   }
+
+   template<typename... Params>
+   static sys_str tsprintf(const sys_char* fmt,
+                    const Params&... args)
+   {
+      std::basic_stringstream<sys_char> ss;
+      return toprintf<decltype(ss)>(ss, fmt, args...).str();
+   }
 }
--- QGL_Model/qgl_mem_helpers.cpp
+++ QGL_Model/qgl_mem_helpers.cpp
@@ -5,30 +5,24 @@
 
 namespace qgl::mem
 {
-   static const sys_char* ALLOC_MESSAGE_FMT = TEXT("Alloc at %x\n");
-   static const sys_char* DEALLOC_MESSAGE_FMT = TEXT("Free at %x\n");
-   static const sys_char* LEAK_MESSAGE_FMT = TEXT("Leak at %x\n");
+   static const sys_char* ALLOC_MESSAGE_FMT = TEXT("Alloc at %\n");
+   static const sys_char* DEALLOC_MESSAGE_FMT = TEXT("Free at %\n");
+   static const sys_char* LEAK_MESSAGE_FMT = TEXT("Leak at %\n");
 
    static constexpr size_t MEM_ALERT_STR_LEN = 24;
 
    void print_mem_leak(uintptr_t address)
    {
-      sys_char outStr[24] = { 0 };
-      swprintf(outStr, 24, LEAK_MESSAGE_FMT, address);
-      qgl::debug_message(outStr);
+      qgl::debug_message(qgl::tsprintf(LEAK_MESSAGE_FMT, address).c_str());
    }
 
    void print_mem_dealloc(uintptr_t address)
    {
-      sys_char outStr[24] = { 0 };
-      swprintf(outStr, 24, DEALLOC_MESSAGE_FMT, address);
-      qgl::debug_message(outStr);
+      qgl::debug_message(qgl::tsprintf(DEALLOC_MESSAGE_FMT, address).c_str());
    }
 
    void print_mem_alloc(uintptr_t address)
    {
-      sys_char outStr[24] = { 0 };
-      swprintf(outStr, 24, ALLOC_MESSAGE_FMT, address);
-      qgl::debug_message(outStr);
+      qgl::debug_message(qgl::tsprintf(ALLOC_MESSAGE_FMT, address).c_str());
    }
 }
